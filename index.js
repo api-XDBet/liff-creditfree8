0ebd75885bc50cc5a2d00c8f6e5cdb1ccdc37ef9
api-XDBet <7be@xd-bet.xyz>
2022-06-03 15:07:33 +0700

    2 files modified

--- index.js
+++ index.js
@@ -5,30 +5,6 @@ import './style.css';
 import liff from '@line/liff';
 
 //Get the button
-var mybutton = document.getElementById('myTopBtn');
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = 'block';
-  } else {
-    mybutton.style.display = 'none';
-  }
-}
-
-myTopBtn.onclick = function () {
-  topFunction();
-};
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 // Profile elements
 // Button elements
 const btnShare = document.getElementById('btnShare');
@@ -540,7 +516,30 @@ async function shareMsg() {
   }
   liff.closeWindow();
 }
-// 11. Add close window
+
+var mybutton = document.getElementById('myTopBtn');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+myTopBtn.onclick = function () {
+  topFunction();
+};
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 // 9. Add event listener to share button
 btnShare.onclick = () => {
