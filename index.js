b5127cf5a7c51484aa2e0f3957f93bcdf5ff4eea
api-XDBet <7be@xd-bet.xyz>
2022-06-12 10:03:05 +0700

    2 files modified

--- index.js
+++ index.js
@@ -453,15 +453,6 @@ function scrollFunction() {
   }
 }
 
-myTopBtn.onclick = function () {
-  topFunction();
-};
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 // 9. Add event listener to share button
 btnShare.onclick = () => {
   shareMsg();
